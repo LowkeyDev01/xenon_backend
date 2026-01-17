e1894c8d26e666f9ba44ba8b2ba8c8b5818e72bf
Kenny <kananaya335@gmail.com>
2026-01-17 17:48:59 +0100

    bruhh

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-app.post('/health', (req, res) => {
+app.get('/health', (req, res) => {
   res.status.send('ts is running!');
 })
 
